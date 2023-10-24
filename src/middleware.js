04c9ad386b6795fb33b160299634cb80b8849a8b
mehedi-hasan95 <mh.me.mehedi@gmail.com>
2023-10-24 17:41:04 +0600

    Make a middleware for if user login they can not visit login or register page

--- src/middleware.js
+++ src/middleware.js
@@ -1,21 +1,7 @@
-import { NextResponse } from "next/server";
-import { JWTVerify } from "./utility/JWTHelper";
+import { AfterLogin, CheckCookieAuth } from "./utility/CheckCookieAuth";
 export async function middleware(req, res) {
-    try {
-        let token = req.cookies.get("token");
-        let payload = await JWTVerify(token["value"]);
-        const requestHeader = new Headers(req.headers);
-        requestHeader.set("email", payload["email"]);
-        requestHeader.set("id", payload["id"]);
-        requestHeader.set("firstName", payload["firstName"]);
-        return NextResponse.next({ request: { headers: requestHeader } });
-    } catch (e) {
-        const requestHeader = new Headers(req.headers);
-        requestHeader.set("email", "0");
-        requestHeader.set("id", "0");
-        requestHeader.set("firstName", "0");
-        return NextResponse.next({
-            request: { headers: requestHeader },
-        });
+    if (req.nextUrl.pathname.startsWith("/user")) {
+        return AfterLogin(req);
     }
+    return CheckCookieAuth(req);
 }
---
+++ src/utility/CheckCookieAuth.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { JWTVerify } from "./JWTHelper";
+
+export async function CheckCookieAuth(req) {
+    try {
+        let token = req.cookies.get("token");
+        let payload = await JWTVerify(token["value"]);
+        const requestHeader = new Headers(req.headers);
+        requestHeader.set("email", payload["email"]);
+        requestHeader.set("id", payload["id"]);
+        requestHeader.set("firstName", payload["firstName"]);
+        return NextResponse.next({ request: { headers: requestHeader } });
+    } catch (e) {
+        const requestHeader = new Headers(req.headers);
+        requestHeader.set("email", "0");
+        requestHeader.set("id", "0");
+        requestHeader.set("firstName", "0");
+        return NextResponse.next({
+            request: { headers: requestHeader },
+        });
+    }
+}
+
+export async function AfterLogin(req) {
+    try {
+        let token = req.cookies.get("token");
+        if (token) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    } catch (e) {
+        return NextResponse.redirect(new URL("/user/login", req.url));
+    }
+}
